603dfbaf2ef6e9d26f00ea047097d7129c80faf6
t dis <thomdisley@gmail.com>
2017-09-16 08:42:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidayTo (holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown (holiday, holiday) {
+function holidayCountdown (holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
 
